ba91c2533ae1c08a0dc011dcdef3be54cb76ba8c
Martin Mohr <martin.mohr@kit.edu>
2016-05-28 21:31:41 +0200

    create new story, changes to data model

--- umklapp/models.py
+++ umklapp/models.py
@@ -5,14 +5,29 @@ from django.contrib.auth.models import User
 
 class Teller(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    corresponding_story = models.ForeignKey('Story', on_delete=models.CASCADE)
+    corresponding_story = models.ForeignKey('Story', on_delete=models.CASCADE,
+                                            related_name="tellers")
     position = models.IntegerField()
 
 class Story(models.Model):
     started_by = models.ForeignKey(User, related_name="started_by", on_delete=models.CASCADE)
-    whose_turn = models.ForeignKey(Teller, on_delete=models.CASCADE)
+    whose_turn = models.IntegerField()
     is_finished = models.BooleanField()
-    tellers = models.ManyToManyField('Teller', related_name="tellers")
+
+    @staticmethod
+    def create_new_story(startUser, participating_users, first_sentence):
+        s = Story(started_by=startUser, is_finished=False, whose_turn=0)
+        s.save()
+        t0 = Teller(user=startUser, corresponding_story=s, position=0)
+        t0.save()
+        firstPart = StoryPart(teller=t0, content=first_sentence)
+        firstPart.save()
+
+        positions = range(1, len(participating_users)+1)
+        for (u,p) in zip(participating_users, positions):
+            t = Teller(user=u, corresponding_story=s, position=p)
+            t.save()
+
 
     def continue_story(self, text):
         myparts = StoryPart.objects.filter(teller__corresponding_story=self)
@@ -25,7 +40,7 @@ class Story(models.Model):
         self.advance_teller()
 
     def advance_teller(self):
-        self.whose_turn = Teller.get(corresponding_story=self, position=self.whose_turn.position % self.tellers.count())
+        self.whose_turn = (self.whose_turn + 1) % self.tellers.count()
         self.save()
 
 
