dfdd72e07934900b43c7eb630c95dbc4fa380031
Martin Mohr <martin.mohr@kit.edu>
2016-05-29 00:28:44 +0200

    fix settings

--- umklapp_site/settings.py
+++ umklapp_site/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 LOGIN_URL = '/accounts/login/'
