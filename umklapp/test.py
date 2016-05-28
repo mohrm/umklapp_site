eaace54d5e7d8d2ebad42cf31cf071a9cf9d3e50
Martin Mohr <martin.mohr@kit.edu>
2016-05-28 21:32:20 +0200

    test case for creating a new story

---
+++ umklapp/test.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.test.utils import override_settings
+
+from umklapp.models import *
+
+class UmklappTestCase(TestCase):
+    def addUsers(self):
+        self.users = []
+        for i in range(0,7):
+            u = User.objects.create_user(
+                "user%d" % i,
+                "test@example.com",
+                "p455w0rd"
+            )
+            self.users.append(u)
+
+class NewStoryTest(UmklappTestCase):
+    def setUp(self):
+        self.addUsers()
+
+    def testNewStory(self):
+        Story.create_new_story(self.users[0], self.users, "first")
+
