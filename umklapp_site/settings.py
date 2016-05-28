af80fa6b23e64ffc2f268e494a4bc1765aa6c21f
Martin Mohr <martin.mohr@kit.edu>
2016-05-29 00:25:49 +0200

    some more settings (for making it work with heroku)

--- umklapp_site/settings.py
+++ umklapp_site/settings.py
@@ -122,7 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static')
+)
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
