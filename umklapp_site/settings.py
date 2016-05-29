d92f953eb51d362f5d76412e7cb072ab7504cd83
Martin Mohr <martin.mohr@kit.edu>
2016-05-29 16:16:10 +0200

    switch language to german

--- umklapp_site/settings.py
+++ umklapp_site/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-DE'
 
 TIME_ZONE = 'UTC'
 
