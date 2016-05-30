262d6ce5ab952a050dc54c74b853b8d5ec76f8cf
Martin Mohr <martin.mohr@kit.edu>
2016-05-31 00:11:41 +0200

    new global wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for umklapp_site project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
+#!/usr/bin/python
 import os
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "umklapp_site.settings")
-
-application = get_wsgi_application()
+from umklapp_site.wsgi import application
