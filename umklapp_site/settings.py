3dbc2aa16567fae70977dccf85eeaa09cb776ec0
Martin Mohr <martin.mohr@kit.edu>
2016-05-31 01:26:08 +0200

    this should work too

--- umklapp_site/settings.py
+++ umklapp_site/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '..', 'wsgi', 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'wsgi', 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
