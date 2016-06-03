2393ce35f0561b171edef24e156ffcdf5e9b9656
Andreas Zwinkau <zwinkau@kit.edu>
2016-06-03 10:08:39 +0200

    Revert "Show git revision on website"
    This reverts commit 05b9e1504c986abe82b546efccaadb2482365717.
    
    Seems to be broken on OpenShift

--- umklapp/templatetags/git_revision.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-with open(".git/refs/heads/master") as fh:
-    GIT_REVISION = fh.read().decode("utf8") or "unknown"
-
-@register.simple_tag
-def git_revision():
-    return GIT_REVISION
