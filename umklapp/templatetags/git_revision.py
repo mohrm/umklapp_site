6e1ccd8a83b799bb15e4572316214decb1e9f607
Martin Mohr <martin.mohr@kit.edu>
2016-06-03 10:44:48 +0200

    hopefully access correct file now

--- umklapp/templatetags/git_revision.py
+++ umklapp/templatetags/git_revision.py
@@ -3,8 +3,14 @@ from django import template
 from django.conf import settings
 register = template.Library()
 
-with open(os.path.join(settings.BASE_DIR, ".git", "refs", "heads", "master")) as fh:
-    GIT_REVISION = fh.read().decode("utf8") or "unknown"
+if 'OPENSHIFT_APP_NAME' in os.environ:
+    OPENSHIFT_APP_NAME = os.environ['OPENSHIFT_APP_NAME']
+    OPENSHIFT_HOMEDIR = os.environ['OPENSHIFT_HOMEDIR']
+    with open(os.path.join(OPENSHIFT_HOMEDIR, "git", OPENSHIFT_APP_NAME + ".git", "refs", "heads", "master")) as fh:
+        GIT_REVISION = fh.read().decode("utf8") or "unknown"
+else:
+    with open(os.path.join(settings.OPENSHIFT_HOMEDIR, ".git", "refs", "heads", "master")) as fh:
+        GIT_REVISION = fh.read().decode("utf8") or "unknown"
 
 @register.simple_tag
 def git_revision():
