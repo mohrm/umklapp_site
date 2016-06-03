2c0da1e6d6f1bfd020d53e5deb3ea755136ddc99
Martin Mohr <martin.mohr@kit.edu>
2016-06-03 10:29:56 +0200

    access proper .git directory

--- umklapp/templatetags/git_revision.py
+++ umklapp/templatetags/git_revision.py
@@ -1,7 +1,9 @@
+import os
 from django import template
+from django.conf import settings
 register = template.Library()
 
-with open(".git/refs/heads/master") as fh:
+with open(os.path.join(settings.BASE_DIR, ".git", "refs", "heads", "master")) as fh:
     GIT_REVISION = fh.read().decode("utf8") or "unknown"
 
 @register.simple_tag
