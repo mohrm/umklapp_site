9d30a4436ac5c23f29dd006c8dcc8672d194ddc1
Martin Mohr <martin.mohr@kit.edu>
2016-06-04 12:07:20 +0200

    backup script

---
+++ backup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rhc ssh umklapp "./app-root/repo/manage.py dumpdata --natural-primary --exclude contenttypes --exclude=auth.Permission" > backups/umklapp-backup-$(date --iso=minutes).json
