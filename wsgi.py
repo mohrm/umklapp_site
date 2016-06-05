ccef201ae3d74f0070e862877c7bb9c238117197
Martin Mohr <martin.mohr@kit.edu>
2016-06-05 23:54:39 +0200

    Revert "hopefully improve .htaccess"
    This reverts commit 8bc67be1b42c4d9dd2aa127b18238f60b2e5f31b.
    
    Revert "Do additinal things to enforce HTTPS"
    
    This reverts commit e4e04c6ebf498c1cbaf807cc64bba402304d3451.
    
    Revert "use correct environment variable"
    
    This reverts commit 2d5c183fc262e280ca69779e33b6888ad44a1130.

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,6 @@
 import os
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-os.environ['HTTPS'] = "on"
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
