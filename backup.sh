b00f6d97260c0b262d15c24c8f80676db3bd21f1
Martin Mohr <martin.mohr@kit.edu>
2016-06-16 11:19:15 +0200

    paramterize backup script

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rhc ssh umklapp "./app-root/repo/manage.py dumpdata --natural-primary --exclude contenttypes --exclude=auth.Permission" > backups/umklapp-backup-$(date --iso=minutes).json
+rhc ssh umklapp "./app-root/repo/manage.py dumpdata --natural-primary --exclude contenttypes --exclude=auth.Permission" > $1/backups/umklapp-backup-$(date --iso=minutes).json
