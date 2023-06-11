9b8de318ece68a21bf4f228670785b05751c15b9
Martin Mohr <martin.mohr@posteo.de>
2023-06-11 15:57:22 +0200

    Update auf Python 3.11 und Django 4.2 (inkl. neuster Versionen der anderen Pakete)

--- umklapp_site/urls.py
+++ umklapp_site/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.conf import settings
 
 from django.contrib import admin
@@ -10,36 +10,36 @@ admin.autodiscover()
 
 urlpatterns = [
 # http://www.voidynullness.net/blog/2014/01/15/raiders-of-the-lost-django-registration-templates/
-    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
-    url(r'^accounts/login/$',
+    re_path(r'^accounts/', include('django_registration.backends.one_step.urls')),
+    re_path(r'^accounts/login/$',
                     auth_views.LoginView.as_view(
                     template_name = 'umklapp/login.html'),
                     name='auth_login'),
-    url(r'^accounts/logout/$',
+    re_path(r'^accounts/logout/$',
                     auth_views.LogoutView.as_view(
                     template_name = 'umklapp/logout.html'),
                     name='auth_logout'),
-    url(r'^accounts/password/change/$',
-                    auth_views.PasswordChangeView,
-                    {'template_name': 'umklapp/password_change_form.html'},
+    re_path(r'^accounts/password/change/$',
+                    auth_views.PasswordChangeView.as_view(
+                    template_name = 'umklapp/password_change_form.html'),
                     name='password_change'),
-    url(r'^accounts/password/change/done/$',
-                    auth_views.PasswordChangeDoneView,
-                    {'template_name': 'umklapp/password_change_done.html'},
+    re_path(r'^accounts/password/change/done/$',
+                    auth_views.PasswordChangeDoneView.as_view(
+                    template_name = 'umklapp/password_change_done.html'),
                     name='password_change_done'),
 
-    url(r'^accounts/password/reset/$',
-                    auth_views.PasswordResetView,
+    re_path(r'^accounts/password/reset/$',
+                    auth_views.PasswordResetView.as_view(),
                     name='password_reset'),
-    url(r'^accounts/password/reset/done/$',
-                    auth_views.PasswordResetDoneView,
+    re_path(r'^accounts/password/reset/done/$',
+                    auth_views.PasswordResetDoneView.as_view(),
                     name='password_reset_done'),
-    url(r'^accounts/password/reset/complete/$',
-                    auth_views.PasswordResetCompleteView,
+    re_path(r'^accounts/password/reset/complete/$',
+                    auth_views.PasswordResetCompleteView.as_view(),
                     name='password_reset_complete'),
-    url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
-                    auth_views.PasswordResetConfirmView,
+    re_path(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
+                    auth_views.PasswordResetConfirmView.as_view(),
                     name='password_reset_confirm'),
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('umklapp.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'', include('umklapp.urls')),
 ]
